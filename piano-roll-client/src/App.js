6c33fe60fbe436eecc3ce8c2eb9c113f2cab30cb
szrharrison <s.z.r.harrison@gmail.com>
2017-05-30 09:39:51 -0400

    Fixed duplicate declaration of Note component in App.js

--- piano-roll-client/src/App.js
+++ piano-roll-client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Note from '../src/components/Note'
 
 import { fetchSong } from './api'
 import Note from './components/Note'
