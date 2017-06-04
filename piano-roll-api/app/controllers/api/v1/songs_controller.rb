61ade440d7a46980e6b1c14d53428ef79951d14d
szrharrison <s.z.r.harrison@gmail.com>
2017-06-04 14:29:07 -0400

    Made it so that the '/api/v1/songs' api endpoint only returns the title and id of the song

--- piano-roll-api/app/controllers/api/v1/songs_controller.rb
+++ piano-roll-api/app/controllers/api/v1/songs_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SongsController < Api::V1::ApplicationController
   before_action :set_song, only: [:show]
 
   def index
-    render json: Song.all
+    render json: Song.all, each_serializer: SongsSerializer
   end
 
   def show
