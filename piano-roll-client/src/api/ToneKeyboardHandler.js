1db6af3a7d4c5dab25f9708348ab8c70fdb67cd9
drumnation <davidmieloch@gmail.com>
2017-07-09 23:45:21 -0400

    refactored / fixed loading error by catching it on line 31

--- piano-roll-client/src/api/ToneKeyboardHandler.js
+++ piano-roll-client/src/api/ToneKeyboardHandler.js
@@ -3,12 +3,11 @@ import _ from 'lodash'
 
 import { sevenOctavePiano } from '../concerns/keyboard'
 
-function notePaths(instrument) {
+const notePaths = instrument => {
   let paths = sevenOctavePiano.reduce( (acc, note) => {
     let mp3Note = note
-    if( note.search('#') !== -1 ) {
-      mp3Note = nextLetter(note[0]) + 'b' + note.substr(2);
-    }
+    if (note.search('#') !== -1)
+      mp3Note = nextLetter(note[0]) + 'b' + note.substr(2)
     return {
       ...acc,
       [note]: `https://raw.githubusercontent.com/drumnation/pre-rendered-soundfont-libs-for-midi-js/master/Compifont_NEW/${instrument}-mp3/${mp3Note}.mp3`
@@ -25,23 +24,15 @@ class Player {
     this.multiPlayer.fadeOut = 0.05
   }
 
-  triggerKey = (noteName, duration) => {
-    this.multiPlayer.start(noteName, "+0.1", 0, duration)
-  }
+  triggerKey = (noteName, duration) => this.multiPlayer.start(noteName, "+0.1", 0, duration)
 
   addNote = async note => {
-    try {
-      let addedNote
-      addedNote = await this.players[note.name].sync().start(note.start_time, 0, note.duration)
-      return addedNote
-    } catch (err) {
-      console.log(err)
-    }
+    try { this.players[note.name].sync().start(note.start_time, 0, note.duration) } 
+    catch (err) {}
   }
 
-  setInstrument = (instrument) => {
-    if(this.instrument !== instrument) {
-      console.log(this.players)
+  setInstrument = instrument => {
+    if (this.instrument !== instrument) {
       this.instrument = instrument
       const notePathsObject = notePaths(instrument)
       this.players = _.mapValues(notePathsObject, notePath => new Tone.Player({
@@ -65,12 +56,12 @@ const player = new Player()
 
 export default player
 
-function nextLetter(s){
-    return s.replace(/([a-zA-Z])[^a-zA-Z]*$/, function(a) {
-        var c = a.charCodeAt(0)
-        switch(c){
+function nextLetter(string) {
+    return string.replace(/([a-zA-Z])[^a-zA-Z]*$/, (formattedString) => {
+        let character = formattedString.charCodeAt(0)
+        switch(character) {
             case 71: return 'A'
-            default: return String.fromCharCode(++c)
+            default: return String.fromCharCode(++character)
         }
     })
 }
